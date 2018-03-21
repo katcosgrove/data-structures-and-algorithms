eac06d72bfc820355b83c8df2e9fd8963c51a61b
Kat Cosgrove <kat.cosgrove@gmail.com>
2018-03-20 23:32:25 -0700

    still trying for one without built in methods

--- challenges/shift-array/shift-array.py
+++ challenges/shift-array/shift-array.py
@@ -1,3 +1,4 @@
+
 # Doesn't work, but also doesn't use any built-in methods. Progress?
 # def insertShiftArray(arr, val):
 #     counter = 0
@@ -5,16 +6,36 @@
 #     for item in arr:
 #         counter += 1
 
+#     counter = counter // 2
+
 #     for i in arr:
-#         if i > counter // 2:
-#             new_list = new_list + arr[i]
-#         elif i == counter // 2:
-#             new_list = new_list + val
+#         if arr[i] > counter:
+#             new_list[i] = new_list + arr[i]
+#         elif arr[i] == counter:
+#             new_list[i] = new_list + val
 #         else:
-#             new_list = new_list + arr[i]
+#             new_list[i] = new_list + arr[i]
 #     print(new_list)
 
-# Doesn't work, and anyway, I don't think slicing lists is allowed
+# insertShiftArray([1, 2, 3, 4], 5)
+
+# Does work, but I don't think len() is allowed
+# def insertShiftArray(arr, val):
+#     counter = 0
+#     new_list = arr + [0]
+
+#     for item in arr:
+#         if counter == len(arr) / 2:
+#             new_list[counter] = val
+#             counter += 1
+
+#         new_list[counter] = item
+#         counter += 1
+#     print(new_list)
+
+# insertShiftArray([1, 2, 3, 4], 5)
+
+# Also doesn't work, and anyway, I don't think slicing lists is allowed
 # def insertShiftArray(arr, val):
 #     counter = 0
 #     new_array = []
