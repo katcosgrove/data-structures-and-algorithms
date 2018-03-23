19cb5fde969a0f7884703addbd0405fb419882c4
Kat Cosgrove <kat.cosgrove@gmail.com>
2018-03-23 12:37:05 -0700

    solved

---
+++ challenges/largest-product/largest_array.py
@@ -0,0 +1,13 @@
+def LargestArray(arr):
+
+    answer = 0
+    counter = 0
+    new_arr = []
+
+    for item in arr:
+        new_arr[counter] = item[0] * item[1]
+        counter += 1
+    for item in new_arr:
+        if item > answer:
+            answer = item
+    return answer
---
+++ challenges/largest-product/test_largest_array.py
@@ -0,0 +1,8 @@
+import largest_array as la
+
+def test_LargestArray():
+    assert la.LargestArray([[1, 2], [3, 4], [5, 6], [7, 8]]) == 56
+
+
+def test_LargestArray():
+    assert la.LargestArray([[0]]) == 0
