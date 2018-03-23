f4cbb2be8c8d495946fca2c01b3ba84d386f7941
Kat Cosgrove <kat.cosgrove@gmail.com>
2018-03-22 22:25:23 -0700

    fixing merge conflict

--- challenges/shift-array/shift_array.py
+++ challenges/shift-array/shift_array.py
@@ -25,56 +25,7 @@
 # insertShiftArray([1, 2, 3, 4, 5, 6, 7], 5)
 
 
-<<<<<<< HEAD
-=======
-# def insertShiftArray(arr, val):
-#     """
-# Arguments: A list of integers and an integer to be added
-# Output: A new list with the new value added in the middle
-# Issues: This one accounts for an even array, but not odd
-#     """
-#     counter = 0
-#     new_list = arr + [0]
-
-#     for item in arr:
-#         if counter == len(arr) / 2:
-#             new_list[counter] = val
-#             counter += 1
-
-#         new_list[counter] = item
-#         counter += 1
-
-#     return(new_list)
-
-def insertShiftArray(arr, val):
-    length = len(arr)
-    new_list = arr + [0]
-    counter = 0
-    if length % 2 == 0:
-        for item in arr:
-            if counter == length / 2:
-                new_list[counter] = val
-                counter += 1
-            new_list[counter] = item
-            counter += 1
-        return new_list
-
-    else:
-        for item in arr:
-            if counter == round(length / 2):
-                counter += 1
-                new_list[counter] = val
-            # counter += 1
-            new_list[counter] = item
-            counter += 1
-        return new_list
-
-
-# insertShiftArray([20, 30, 59, 70], 200)
-
-
 # Doesn't work, but also doesn't use any built-in methods. Progress?
->>>>>>> d725ee611c6e52472a6cdcd20c6ac418fb60554c
 # def insertShiftArray(arr, val):
 #     """
 # Arguments: A list of integers and an integer to be added
--- challenges/shift-array/test_shift_array.py
+++ challenges/shift-array/test_shift_array.py
@@ -3,7 +3,6 @@ import shift_array as sa
 
 def test_insertShiftArray():
     assert sa.insertShiftArray([1, 2, 3, 4], 5) == [1, 2, 5, 3, 4]
-<<<<<<< HEAD
 
 
 def test_empty_insertShiftArray():
@@ -12,8 +11,3 @@ def test_empty_insertShiftArray():
 
 def test_odd_insertShiftArray():
     assert sa.insertShiftArray([1, 2, 3], 5) == [1, 2, 5, 3]
-=======
-    assert sa.insertShiftArray([1, 2, 3, 4, 5, 6, 7, 8], 5) == [1, 2, 3, 4, 5, 5, 6, 7, 8]
-    assert sa.insertShiftArray([1, 2, 3], 5) == [1, 5, 2, 3]
-    assert sa.insertShiftArray([], 2) == 2
->>>>>>> d725ee611c6e52472a6cdcd20c6ac418fb60554c
